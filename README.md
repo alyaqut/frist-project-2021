b5ce609de5be461f16ba4ea54677ee3125658629
alyaqut <118297104+alyaqut@users.noreply.github.com>
2023-01-07 18:40:30 +0300

    frist project 2021

---
+++ README.md
@@ -0,0 +1,3 @@
+# html-css
+# html-css
+# html-css
---
+++ test11.js
@@ -0,0 +1,11 @@
+/*textBox = document.querySelector("textarea"),
+btn     = document.querySelector(".btn-post");
+function textTospeech(text) {
+    let uttu = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(uttu);
+}
+btn.addEventListener("click" , ()=> {
+    if(textBox.value != "") {
+        textTospeech(textBox.value);
+    }
+})*/
